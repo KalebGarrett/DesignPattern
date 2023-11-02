1d6709956e166f16d880707114281111a886abff
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:24:40 -0400

    Added decrease volume command

---
+++ DesignPattern.Command/Command/DecreaseVolume.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPattern.Command.Command;
+
+public class DecreaseVolume : ICommand
+{
+    public Tv Tv { get; set; }
+    
+    public DecreaseVolume(Tv tv)
+    {
+        Tv = tv;
+    }
+
+    public void Execute()
+    {
+        Tv.DecreaseVolume();
+    }
+}
