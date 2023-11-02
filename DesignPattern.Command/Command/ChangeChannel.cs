b9cc1404e71bff3c6806fb3234089b51def15455
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:21:22 -0400

    Added change channel command

---
+++ DesignPattern.Command/Command/ChangeChannel.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPattern.Command.Command;
+
+public class ChangeChannel : ICommand
+{
+    public Tv Tv { get; set; }
+    public int Channel { get; set; }
+    
+    public ChangeChannel(Tv tv, int channel)
+    {
+        Tv = tv;
+        Channel = channel;
+    }
+
+    public void Execute()
+    {
+        Tv.ChangeChannel(Channel);
+    }
+}
