4c45a418bf1ff6f3e7fa75696bb794f4679f4831
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:26:55 -0400

    Added invoker/command base class

---
+++ DesignPattern.Command/TvRemote.cs
@@ -0,0 +1,19 @@
+﻿using DesignPattern.Command.Command;
+
+namespace DesignPattern.Command;
+
+// Invoker/Command Base
+public class TvRemote
+{
+    public ICommand Command { get; set; }
+
+    public void SetCommand(ICommand command)
+    {
+        Command = command;
+    }
+
+    public void PressButton()
+    {
+        Command.Execute();
+    }
+}
