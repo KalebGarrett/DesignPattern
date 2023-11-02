f52e51b4e844eff4a67910066bd7e2b99c84fc45
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:19:37 -0400

    Added turn off tv command

---
+++ DesignPattern.Command/Command/TurnOffTv.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPattern.Command.Command;
+
+public class TurnOffTv : ICommand
+{
+    public Tv Tv { get; set; }
+
+    public TurnOffTv(Tv tv)
+    {
+        Tv = tv;
+    }
+    
+    public void Execute()
+    {
+        Tv.TurnOff();
+    }
+}
