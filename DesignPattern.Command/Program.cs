60c22da1ffe8b58b8db1f064ffe4e076a24ced39
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:34:23 -0400

    Updated program class

--- DesignPattern.Command/Program.cs
+++ DesignPattern.Command/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DesignPattern.Command;
+using DesignPattern.Command.Command;
 
-Console.WriteLine("Hello, World!");
+var tv = new Tv();
+
+var turnOnCommand = new TurnOnTv(tv);
+var turnOffCommand = new TurnOffTv(tv);
+var changeChannelCommand = new ChangeChannel(tv, 247);
+var increaseVolumeCommand = new IncreaseVolume(tv);
+var decreaseVolumeCommand = new DecreaseVolume(tv);
+
+var remoteControl = new TvRemote();
+
+remoteControl.SetCommand(turnOnCommand);
+remoteControl.PressButton();
+
+remoteControl.SetCommand(changeChannelCommand);
+remoteControl.PressButton();
+
+remoteControl.SetCommand(increaseVolumeCommand);
+remoteControl.PressButton();
+remoteControl.PressButton();
+
+remoteControl.SetCommand(decreaseVolumeCommand);
+remoteControl.PressButton();
+remoteControl.PressButton();
+remoteControl.PressButton();
+remoteControl.PressButton();
+remoteControl.PressButton();
+remoteControl.PressButton();
+
+var currentVolume = tv.GetVolume();
+Console.WriteLine($"\nCurrent TV volume: {currentVolume}\n");
+
+remoteControl.SetCommand(turnOffCommand);
+remoteControl.PressButton();
+
+Console.ReadKey();
