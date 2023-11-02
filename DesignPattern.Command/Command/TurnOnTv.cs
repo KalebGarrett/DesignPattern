cb8634d224391381d3c7d05ea523eeadbc22edde
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-02 17:18:40 -0400

    Added turn on tv command

---
+++ DesignPattern.Command/Command/TurnOnTv.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace DesignPattern.Command.Command;
+
+public class TurnOnTv : ICommand
+{
+    public Tv Tv { get; set; }
+
+    public TurnOnTv(Tv tv)
+    {
+        Tv = tv;
+    }
+
+    public void Execute()
+    {
+        Tv.TurnOn();
+    }
+}
