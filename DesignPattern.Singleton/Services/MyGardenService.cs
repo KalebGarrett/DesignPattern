c208bd5e1c96aa26890342245d053d8d500da7f8
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-03 12:28:04 -0400

    Added my garden service

---
+++ DesignPattern.Singleton/Services/MyGardenService.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using DesignPattern.Singleton.Models;
+
+namespace DesignPattern.Singleton.Services;
+
+public class MyGardenService
+{
+    private static MyGardenService? _instance;
+    private MyGardenService() { }
+
+    public static MyGardenService GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new MyGardenService();
+        }
+
+        return _instance;
+    }
+
+    public async Task<List<MyGarden>> GetAll()
+    {
+
+        using var client = new HttpClient();
+        var result = await client.GetAsync("https://mygardenapi.azurewebsites.net/plants");
+        if (result.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"HTTP Status Code: {result.StatusCode}\n");
+            var json = await result.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<MyGarden>>(json)!;
+        }
+        else
+        {
+            Console.WriteLine($"HTTP Status Code: {result.StatusCode}");
+        }
+
+        return new List<MyGarden>();
+    }
+}
