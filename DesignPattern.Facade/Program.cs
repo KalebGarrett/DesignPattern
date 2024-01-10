e32c3c9efe923f7bdc51d6fdd739fddeeb653e57
Kaleb Garrett <kalebagarrett@gmail.com>
2024-01-10 15:06:48 -0500

    Configured app

--- DesignPattern.Facade/Program.cs
+++ DesignPattern.Facade/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DesignPattern.Facade;
+using DesignPattern.Facade.Services;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("---------------CLIENT ORDERS FOR SUSHI---------------");
+
+var kyotoExpress = new KyotoExpressFacade
+(
+    new SushiService(), 
+    new HibachiService(), 
+    new RamenService()
+);
+
+kyotoExpress.GetCookedSushi();
+kyotoExpress.GetRawSushi();
+
+Console.WriteLine("---------------CLIENT ORDERS FOR HIBACHI---------------");
+
+kyotoExpress.GetSteakHibachi();
+kyotoExpress.GetChickenHibachi();
+kyotoExpress.GetPorkHibachi();
+
+Console.WriteLine("---------------CLIENT ORDERS FOR RAMEN---------------");
+
+kyotoExpress.GetBeefRamen();
+kyotoExpress.GetChickenRamen();
+kyotoExpress.GetShrimpRamen();
